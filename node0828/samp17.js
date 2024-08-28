0e8f57324d9ec911bffcf079b1c22f790276dcf8
HyukJin98 <dlgurwls4321@gmail.com>
2024-08-28 18:34:09 +0900

    0828 update

---
+++ node0828/samp17.js
@@ -0,0 +1,9 @@
+
+// 비동기 처리
+setTimeout(() =>{
+    console.log('todo: First work!');
+}, 3000);
+
+setTimeout(() => {
+    console.log('todo: Second work!');
+}, 2000);
---
+++ node0828/samp20.js
@@ -0,0 +1,8 @@
+
+console.log(0);
+
+setTimeout(() => {
+    console.log('Hello');
+}, 0);
+
+console.log(1);
